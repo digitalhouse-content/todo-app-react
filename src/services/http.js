2f7209200f8f2fc207577af076be54503bb3162d
Humberto Rivero <humbertorivero9@gmail.com>
2024-06-04 19:44:03 -0300

    add endpoint AWS

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,7 @@
 // Agregar a la base
 
-const URL_BASE = "http://localhost:8080/tasks"; // aws
+const URL_BASE = "http://18.219.160.0:8080/tasks"; // aws
+// const URL_BASE = "http://localhost:3005/todos";
 
 // Defaul headers for fetch
 const defaultHeaders = {
